3a8fd3b3323d842d91992c0eef9fba8bffad1b97
rostmart <mart0408@ukr.net>
2022-06-08 21:42:02 +0200

    modified server index js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const redis = require("redis");
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000,
+  retry_strategy: () => 1000
 });
 const redisPublisher = redisClient.duplicate();
 
