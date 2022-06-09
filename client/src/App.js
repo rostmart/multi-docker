72c498803759d0b287637f354ff35f1264c84980
rostmart <mart0408@ukr.net>
2022-06-09 22:32:55 +0200

    test deployment next

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Homik</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
